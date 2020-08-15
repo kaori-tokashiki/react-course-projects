4ece83746e4b6823aa4207d44144c442270aedc5
kaoritokashiki <kaori-t@weseek.co.jp>
2020-08-15 20:55:55 +0900

    push big changes in these files

--- indecision-app/src/components/AddOption.js
+++ indecision-app/src/components/AddOption.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 
 export default class AddOption extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            error: undefined,
-        };
-        this.handleAddOption = this.handleAddOption.bind(this);
+    state = {
+        error: undefined
     }
-    handleAddOption(e){
+    // constructor(props){
+    //     super(props);
+    //     this.handleAddOption = this.handleAddOption.bind(this);
+    // }
+    handleAddOption = (e) => {
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
