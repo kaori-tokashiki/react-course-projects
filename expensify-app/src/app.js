7f20c6382d8b23814b06c4a2eb755270dbdae26a
kaoritokashiki <kaori-t@weseek.co.jp>
2020-08-30 17:43:03 +0900

    done settiong app budget app-76

---
+++ expensify-app/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css';
+import './styles/styles.scss'
+
+
+ReactDOM.render(<p>This is my boikerplate</p>, document.getElementById('app'));
