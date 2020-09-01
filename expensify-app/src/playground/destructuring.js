9950f0b38a61e8f1c4d6c591a12e8122bfb96e29
kaoritokashiki <kaori-t@weseek.co.jp>
2020-09-01 23:19:33 +0900

    done section 10, 90

---
+++ expensify-app/src/playground/destructuring.js
@@ -0,0 +1,44 @@
+// const person = {
+//     name: 'Andrew',
+//     age: 27,
+//     location: {
+//         city: 'Philadelphia',
+//         temp: 88
+//     }
+// };
+
+// const {name: firstName = 'Anonymous', age} = person;
+
+
+// console.log(`${firstName} is ${age}.`);
+
+// const {city, temp:tempearature} = person.location
+
+// if(city && tempearature){
+//     console.log(`It's ${tempearature} in ${city}.`);
+// }
+
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
+
+// const { name } = book.publisher
+// const {name: publisherName = 'Self-Published'} = book.publisher
+
+// console.log(publisherName);
+
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+
+const [, city, state = 'New York'] = address;
+console.log(`You are in ${city} ${state}`);
+
+const item = ['Coffee(iced)', '$2.00', '$2.50', '$2.75'];
+
+const [itemName, ,mediumPrice,] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
---
+++ expensify-app/src/playground/redux-101.js
@@ -0,0 +1,62 @@
+import { createStore } from 'redux';
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy,
+});
+
+const decrementCount = ({decrementBy = 1} = {}) => ({
+    type: 'DECREMENT',
+    decrementBy,
+});
+const setCount = ({ count }) => ({
+    type: 'SET',
+    count,
+});
+const resetCount = () => ({
+    type: 'RESET',
+});
+
+const store = createStore((state = { count: 0 }, action) => {
+    // console.log(action);
+    switch (action.type){
+        case 'INCREMENT':
+            return {
+                count: state.count + action.incrementBy
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - action.decrementBy
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
+            };
+        default: 
+        return state;
+    }
+});
+
+const unsubscribe = store.subscribe(()=>{
+    console.log(store.getState());
+})
+
+
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(incrementCount());
+
+store.dispatch(setCount({ count: 101 }));
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount());
+store.dispatch(decrementCount({ decrementBy: 10 }));
+// store.dispatch({
+//     type: 'SET',
+//     count: 101
+// });
+
+
